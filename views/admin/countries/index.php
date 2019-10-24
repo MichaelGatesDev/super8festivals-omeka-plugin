f341095cb2d20135e72f5aee6dcd314cdb644fb3
Michael Gates <michaelgatesdev@gmail.com>
2019-10-23 23:33:40 -0400

    Fixed broken URLs

--- views/admin/countries/index.php
+++ views/admin/countries/index.php
@@ -6,7 +6,7 @@ echo head($head);
 ?>
 
 
-<?php echo $this->partial('__components/button.php', array('url' => 'super-eight-festivals/countries/add', 'text' => 'Add Country')); ?>
+<?php echo $this->partial('__components/button.php', array('url' => 'add', 'text' => 'Add Country')); ?>
 
 <?php
 $records = get_records("SuperEightFestivalsCountry");
@@ -14,7 +14,7 @@ echo $this->partial('__components/records/record-view.php',
     array(
         'records' => $records,
         'msgNoRecordsFound' => 'No countries found.',
-        'urlAddRecord' => 'super-eight-festivals/countries/add',
+        'urlAddRecord' => 'add',
         'msgAskAddRecord' => 'Add a country.',
         'viewPartial' => $this->partial('__components/records/table-view.php', array(
             'tableHeaders' => array('Name', 'Latitude', 'Longitude'),

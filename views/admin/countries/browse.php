25ae100aaeffc672dea4467b47e493f04488ef61
Michael Gates <michaelgatesdev@gmail.com>
2019-10-28 21:55:19 -0400

    fix typo

--- views/admin/countries/browse.php
+++ views/admin/countries/browse.php
@@ -8,7 +8,7 @@ echo head(array(
 <?php echo flash(); ?>
 
 <!-- 'Add City' Button -->
-<?php echo $this->partial('__components/button.php', array('url' => 'add', 'text' => 'Add City')); ?>
+<?php echo $this->partial('__components/button.php', array('url' => 'add', 'text' => 'Add Country')); ?>
 
 <!-- Table of countries (editable) -->
 <?php echo $this->partial('__components/tables/editable-record-table.php', array(

a4d01da6da2f8c2d8d0f165faa2593c7a19e7a47
Michael Gates <michaelgatesdev@gmail.com>
2019-10-30 21:40:22 -0400

    Add template to about

--- views/public/about/index.php
+++ views/public/about/index.php
@@ -7,22 +7,71 @@ echo head($head);
 
 <?php echo flash(); ?>
 
-<section>
+<section class="container">
 
+    <hr/>
+    <div class="row" style="padding: 1em 0;">
+        <div class="col horizontal-align">
+            <h2>The Book</h2>
+        </div>
+    </div>
+    <hr/>
     <div class="row">
         <div class="col">
-            <h2 class="section-title">About the Author</h2>
+            <div class="row">
+                <!--Book Image-->
+                <div class="col-3 horizontal-align">
+                    <img src="https://www.hachettebookgroup.com/wp-content/uploads/2019/05/national-bestseller-calypso.jpg" alt="" width="250" height="375">
+                </div>
+                <!--Book Info-->
+                <div class="col">
+                    <div class="row">
+                        <h3>My Cool Title</h3>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate ut pharetra sit amet aliquam id. In vitae turpis massa sed elementum tempus. Aliquet enim tortor at auctor urna. Pulvinar pellentesque
+                            habitant morbi tristique senectus et netus et. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Vivamus at augue eget arcu dictum varius. Orci porta non pulvinar neque laoreet suspendisse interdum. Mattis nunc sed blandit libero volutpat sed cras. Pellentesque
+                            elit eget gravida cum sociis natoque. Tristique sollicitudin nibh sit amet commodo nulla. At auctor urna nunc id. Bibendum est ultricies integer quis auctor elit sed vulputate. Viverra ipsum nunc aliquet bibendum. Tortor pretium viverra suspendisse potenti nullam ac
+                            tortor vitae purus. Volutpat odio facilisis mauris sit. Duis ultricies lacus sed turpis tincidunt. Interdum velit laoreet id donec ultrices tincidunt. Rhoncus mattis rhoncus urna neque viverra justo.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
+    <hr/>
+    <div class="row" style="padding: 1em 0;">
+        <div class="col horizontal-align">
+            <h2>The Author</h2>
+        </div>
+    </div>
+    <hr/>
     <div class="row">
         <div class="col">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div class="row">
+                <!--Book Info-->
+                <div class="col">
+                    <div class="row">
+                        <h3>FirstName LastName</h3>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate ut pharetra sit amet aliquam id. In vitae turpis massa sed elementum tempus. Aliquet enim tortor at auctor urna. Pulvinar pellentesque
+                            habitant morbi tristique senectus et netus et. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Vivamus at augue eget arcu dictum varius. Orci porta non pulvinar neque laoreet suspendisse interdum. Mattis nunc sed blandit libero volutpat sed cras. Pellentesque
+                            elit eget gravida cum sociis natoque. Tristique sollicitudin nibh sit amet commodo nulla. At auctor urna nunc id. Bibendum est ultricies integer quis auctor elit sed vulputate. Viverra ipsum nunc aliquet bibendum. Tortor pretium viverra suspendisse potenti nullam ac
+                            tortor vitae purus. Volutpat odio facilisis mauris sit. Duis ultricies lacus sed turpis tincidunt. Interdum velit laoreet id donec ultrices tincidunt. Rhoncus mattis rhoncus urna neque viverra justo.
+                        </p>
+                    </div>
+                </div>
+                <!--Book Image-->
+                <div class="col-3 horizontal-align">
+                    <img src="http://mho6nxweoblkn351jmpbb12y.wpengine.netdna-cdn.com/wp-content/uploads/2018/09/person-placeholder.jpg" alt="" width="250" height="250">
+                </div>
+            </div>
         </div>
     </div>
 

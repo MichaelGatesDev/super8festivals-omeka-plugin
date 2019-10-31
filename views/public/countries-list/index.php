2a789c1c0ad7bdc78ae442e39b1ed00e975f5723
Michael Gates <michaelgatesdev@gmail.com>
2019-10-31 09:28:52 -0400

    wrap inside section and horizontal align

--- views/public/countries-list/index.php
+++ views/public/countries-list/index.php
@@ -14,15 +14,17 @@ $countries = get_db()->getTable("SuperEightFestivalsCountry")->findAll();
 sort($countries);
 ?>
 
-<h3>Festival Countries:</h3>
-<ul class="countries-list">
-    <?php foreach ($countries as $country): ?>
-        <li>
-            <a href="<?= $this->url('countries/' . str_replace(" ", "-", strtolower($country->name))); ?>">
-                <?php echo $country->name ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<section id="countries-list">
+    <h3>Festival Countries:</h3>
+    <ul class="countries-list">
+        <?php foreach ($countries as $country): ?>
+            <li>
+                <a href="<?= $this->url('countries/' . str_replace(" ", "-", strtolower($country->name))); ?>">
+                    <?php echo $country->name ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
 
 <?php echo foot(); ?>

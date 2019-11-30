45f46404dd5bf9e2acde363a8f008f53da4b46ba
Michael Gates <michaelgatesdev@gmail.com>
2019-11-30 15:48:41 -0500

    Add card/list view for countries

--- views/public/countries-list/index.php
+++ views/public/countries-list/index.php
@@ -16,15 +16,35 @@ sort($countries);
 
 <section class="container-fluid" id="countries-list">
 
-    <div class="row">
+    <div class="row mb-3">
         <div class="col">
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary active" id="gridButton">Grid View</button>
+                <button type="button" class="btn btn-secondary" id="listButton">List View</button>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-6">
-            Images can go here
+
+
+    <div class="row" id="grid">
+        <div class="col d-flex justify-content-lg-start justify-content-center">
+            <div class="card-deck d-flex justify-content-lg-start justify-content-center">
+                <?php foreach ($countries as $country): ?>
+                    <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                        <img class="card-img-top" src="https://placehold.it/280x140/abc" alt="Card image cap">
+                        <div class="card-body">
+                            <h3 class="card-title"><?= $country->name; ?></h3>
+                            <!--                            <p class="card-text">This is a longer card <br> with <br> supporting <br> text below <br> as a natural lead-in to additional content. This content is a little bit longer.</p>-->
+                            <p class="card-text"><small class="text-muted">(0) Festivals</small></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
-        <div class="col-6">
+    </div>
+
+    <div class="row d-none" id="list">
+        <div class="col">
             <h2>Festival Countries:</h2>
             <ul class="countries-list">
                 <?php foreach ($countries as $country): ?>
@@ -37,6 +57,37 @@ sort($countries);
             </ul>
         </div>
     </div>
+
+    <script>
+        $(document).ready(() => {
+            const listButton = $('#listButton');
+            const gridButton = $('#gridButton');
+            const listElem = $('#list');
+            const gridElem = $('#grid');
+
+            gridButton.click(() => grid());
+            listButton.click(() => list());
+
+            const grid = () => {
+                gridButton.addClass('active');
+                listButton.removeClass('active');
+                /// if grid hidden, unhide
+                if (gridElem.hasClass('d-none')) gridElem.removeClass('d-none');
+                // if list not hidden, hide
+                if (!listElem.hasClass('d-none')) listElem.addClass('d-none');
+            };
+
+            const list = () => {
+                gridButton.removeClass('active');
+                listButton.addClass('active');
+                /// if grid hidden, unhide
+                if (listElem.hasClass('d-none')) listElem.removeClass('d-none');
+                // if list not hidden, hide
+                if (!gridElem.hasClass('d-none')) gridElem.addClass('d-none');
+            };
+        });
+    </script>
+
 </section>
 
 <?php echo foot(); ?>

0e09121f07b7818873aa56433d03b1a6c5cf98c6
Michael Gates <michaelgatesdev@gmail.com>
2019-11-30 17:32:39 -0500

    Add history view

---
+++ controllers/HistoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+class SuperEightFestivals_HistoryController extends Omeka_Controller_AbstractActionController
+{
+    public function init()
+    {
+    }
+
+    public function indexAction()
+    {
+        return;
+    }
+}

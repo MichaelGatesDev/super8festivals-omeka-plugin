6d002327a3a19b7dad3b2c0a27238b46c3b69136
Michael Gates <michaelgatesdev@gmail.com>
2019-12-01 22:44:15 -0500

    Capitalize country names

--- views/public/countries-list/index.php
+++ views/public/countries-list/index.php
@@ -34,8 +34,7 @@ sort($countries);
                     <div class="card mb-4" style="min-width: 280px; max-width: 240px;">
                         <img class="card-img-top" src="https://placehold.it/280x140/abc" alt="Card image cap">
                         <div class="card-body">
-                            <h3 class="card-title"><?= $country->name; ?></h3>
-                            <!--                            <p class="card-text">This is a longer card <br> with <br> supporting <br> text below <br> as a natural lead-in to additional content. This content is a little bit longer.</p>-->
+                            <h3 class="card-title text-capitalize"><?= $country->name; ?></h3>
                             <p class="card-text"><small class="text-muted">(0) Festivals</small></p>
                             <a href="<?= $this->url('countries/' . str_replace(" ", "-", strtolower($country->name))); ?>" class="stretched-link"></a>
                         </div>
@@ -50,7 +49,7 @@ sort($countries);
             <h2>Festival Countries</h2>
             <ul class="countries-list">
                 <?php foreach ($countries as $country): ?>
-                    <li>
+                    <li class="text-capitalize">
                         <a href="<?= $this->url('countries/' . str_replace(" ", "-", strtolower($country->name))); ?>">
                             <?php echo $country->name ?>
                         </a>

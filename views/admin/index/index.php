4422c1d9c44c95ad80b3c885ebd5fe3d2143695a
Michael Gates <michaelgatesdev@gmail.com>
2019-12-03 12:56:21 -0500

    Add CP header

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -6,6 +6,8 @@ echo head(array(
 
 <?php echo flash(); ?>
 
+<h2>Super 8 Festivals Control Panel</h2>
+
 <?php echo $this->partial('__components/button.php', array('url' => 'super-eight-festivals/countries/', 'text' => 'Countries')); ?>
 <?php echo $this->partial('__components/button.php', array('url' => 'super-eight-festivals/cities/', 'text' => 'Cities')); ?>
 <?php echo $this->partial('__components/button.php', array('url' => 'super-eight-festivals/festivals/', 'text' => 'Festivals')); ?>

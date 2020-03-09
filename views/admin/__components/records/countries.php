9148cf1568eeca41e089e6b977f3e7a83d26736a
Michael Gates <michaelgatesdev@gmail.com>
2020-03-08 20:18:49 -0400

    Clean up 'countries' section

---
+++ views/admin/__components/records/countries.php
@@ -0,0 +1,32 @@
+<style>
+    #countries {
+        margin-bottom: 1em;
+        padding: 0;
+    }
+
+    .title {
+        text-transform: capitalize;
+    }
+
+    td .button {
+        margin: 0;
+    }
+
+    tr td:first-child {
+        width: 100%;
+    }
+
+    table {
+        margin-bottom: 5em;
+    }
+
+</style>
+<table id="countries">
+    <?php foreach ($countries as $country): ?>
+        <tr>
+            <td><a class="title" href="/admin/super-eight-festivals/countries/<?= $country->name; ?>"><?= $country->name ?></a></td>
+            <td><a class="button blue" href="/admin/super-eight-festivals/countries/<?= $country->name; ?>/edit">Edit</a></td>
+            <td><a class="button red" href="/admin/super-eight-festivals/countries/<?= $country->name; ?>/delete">Delete</a></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
--- views/admin/countries/index.php
+++ views/admin/countries/index.php
@@ -1,4 +1,5 @@
 <?php
+queue_css_file("admin");
 echo head(array(
     'title' => 'Countries',
 ));
@@ -10,37 +11,18 @@ echo head(array(
 <?= $this->partial("__components/breadcrumbs.php"); ?>
 
 <style>
-    .title {
-        text-transform: capitalize;
-    }
-
-    #countries {
-        margin: 0;
-        padding: 0;
-        display: block;
-    }
-
-    .country {
-        font-size: 18px;
-        list-style-type: none;
-    }
-
-    .country a, .country a:visited {
-        color: #c76941;
-    }
-
-    .country a:hover {
-        color: #e88347;
-    }
 </style>
 
-<a class="button" href="/admin/super-eight-festivals/countries/add">Add Country</a>
 
-<ul id="countries">
-    <?php foreach (get_all_countries() as $country): ?>
-        <li class="country title"><a href="/admin/super-eight-festivals/countries/<?= $country->name; ?>"><?= $country->name ?></a></li>
-    <?php endforeach; ?>
-</ul>
+<div class="records-section">
+    <?php $countries = get_all_countries(); ?>
+    <?php if (count($countries) == 0): ?>
+        <p>There are no countries available.</p>
+    <?php else: ?>
+        <?= $this->partial("__components/records/countries.php", array('countries' => $countries)); ?>
+    <?php endif; ?>
+    <a class="button green" href="/admin/super-eight-festivals/countries/<?= $country->name ?>/banners/add">Add Country</a>
+</div>
 
 
 <?php echo foot(); ?>

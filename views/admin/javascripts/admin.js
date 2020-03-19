7ba01766fca548f63baf144b2c5ab7fab032f373
Michael Gates <michaelgatesdev@gmail.com>
2020-03-18 22:38:46 -0400

    Constrain preview instead of distorting it

--- views/admin/javascripts/admin.js
+++ views/admin/javascripts/admin.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
             const reader = new FileReader();
             reader.onload = function (e) {
                 $('#file + embed').remove();
-                $('#file').after('<embed src="' + e.target.result + '" width="450" height="300">');
+                $('#file').after('<embed src="' + e.target.result + '" width="450" height="300" style="object-fit: cover;">');
             };
             reader.readAsDataURL(this.files[0]);
         }

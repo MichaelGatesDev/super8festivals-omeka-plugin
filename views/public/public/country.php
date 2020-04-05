4574a4299ef09e9b86419f8475a94ef806336bbd
Michael Gates <michaelgatesdev@gmail.com>
2020-04-04 23:06:18 -0400

    Simple 1 column layout (for now)

--- views/public/public/country.php
+++ views/public/public/country.php
@@ -20,38 +20,34 @@ $cities = get_all_cities_in_country($country->id);
 
 <section class="container-fluid" id="countries-list">
 
-    <div class="container">
-        <div class="row mb-4">
-            <div class="col-4">
-                <img src="<?= $banner != null ? get_relative_path($banner->get_thumbnail_path()) : "https://placehold.it/280x140/abc" ?>" class="img-fluid img-thumbnail" alt="Responsive image" width="300"/>
+    <div class="container py-2 d-flex flex-column align-items-center">
+        <div class="row py-2">
+            <div class="col">
+                <img src="<?= $banner != null ? get_relative_path($banner->get_thumbnail_path()) : "https://placehold.it/280x140/abc" ?>" class="img-fluid img-thumbnail" alt="Responsive image" style="height: 300px;"/>
             </div>
-            <div class="col-8 d-flex justify-content-start align-items-center" style="vertical-align: middle">
-                <h2 class="mb-0 title"><?= $country->name; ?></h2>
+        </div>
+        <div class="row py-2">
+            <div class="col">
+                <h3 class="my-4">Festival Cities in <?= $country->name ?></h3>
             </div>
         </div>
         <div class="row">
-            <div class="col d-flex flex-column align-items-center">
-                <div class="row">
-                    <div class="col">
-                        <h3>Cities</h3>
+            <?php foreach ($cities as $city): ?>
+                <?php
+                $festivals = get_all_festivals_in_city($city->id);
+                ?>
+                <div class="col">
+                    <div class="card mb-4" style="width: 250px; height: 150px;">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $city->name; ?></h5>
+                            <p class="card-text">
+                                Festivals: <?= count($festivals); ?>
+                            </p>
+                            <a href="/countries/<?= urlencode($country->name); ?>/cities/<?= urlencode($city->name); ?>" class="btn btn-primary w-100">Explore</a>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <ul>
-                        <?php foreach ($cities as $city): ?>
-                            <?php
-                            $festivals = get_all_festivals_in_city($city->id);
-                            ?>
-                            <li class="city pb-0">
-                                <a class="title" href="/countries/<?= urlencode($country->name); ?>/cities/<?= urlencode($city->name); ?>"><?= $city->name; ?></a>
-                                <div class="content">
-                                    <p><?= count($festivals); ?> Festival(s)</p>
-                                </div>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 

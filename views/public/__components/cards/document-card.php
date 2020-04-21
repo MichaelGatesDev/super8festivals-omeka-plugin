a753db6c721970df4d46456b894b31bc79acce7c
Michael Gates <michaelgatesdev@gmail.com>
2020-04-21 10:58:39 -0400

    Use cities are primary organizer instead of countries

--- views/public/__components/cards/document-card.php
+++ views/public/__components/cards/document-card.php
@@ -8,6 +8,10 @@
             <span style="font-weight: bold">Description:</span>
             <span><?= strlen($document->description) > 0 ? $document->description : "N/A"; ?></span>
         </p>
+        <p class="card-title mb-0">
+            <span style="font-weight: bold">Contributor:</span>
+            <span><?= $document->get_contributor() ? $document->get_contributor()->get_display_name() : "N/A"; ?></span>
+        </p>
         <?php if (strlen($document->file_url_web) > 0): ?>
             <a href="<?= $document->file_url_web; ?>" class="stretched-link" target="_blank"></a>
         <?php else: ?>

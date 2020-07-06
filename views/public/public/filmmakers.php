6fe669624774b7ae0f878b8c49a0597554a2f569
Michael Gates <michaelgatesdev@gmail.com>
2020-07-06 19:07:05 -0400

    Update filmmaker sgrid

--- views/public/public/filmmakers.php
+++ views/public/public/filmmakers.php
@@ -10,29 +10,28 @@ echo head($head);
 <style>
 </style>
 
-<section class="container-fluid" id="countries-list">
+<section class="container" id="countries-list">
 
-    <div class="container py-2">
-        <div class="row">
-            <!--Filmmakers-->
-            <div class="col">
-                <h2 class="mb-4">Filmmakers</h2>
-                <?php if (count($records = get_all_filmmakers()) > 0): ?>
-                    <div class="row">
-                        <div class="col">
-                            <div class="card-columns">
-                                <?php foreach ($records as $record): ?>
-                                    <a href="/filmmakers/<?= $record->id; ?>" style="color: black;">
-                                        <?= $this->partial("__components/cards/person-card.php", array("person" => $record)); ?>
-                                    </a>
-                                <?php endforeach; ?>
-                            </div>
+    <div class="row">
+        <div class="col">
+            <h2 class="my-4">Filmmakers</h2>
+        </div>
+    </div>
+
+    <div class="row mb-5">
+        <div class="col">
+            <?php if (count($records = get_all_filmmakers()) > 0): ?>
+                <?php foreach ($records as $filmmaker): ?>
+                    <div class="card d-inline-block m-4">
+                        <div class="card-body">
+                            <h5 class="card-title text-capitalize"><?= $filmmaker->get_display_name(); ?></h5>
                         </div>
+                        <a href="/filmmakers/<?= $filmmaker->id; ?>" class="stretched-link"></a>
                     </div>
-                <?php else: ?>
-                    <p>There are no filmmakers here yet.</p>
-                <?php endif; ?>
-            </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>There are no filmmakers here yet.</p>
+            <?php endif; ?>
         </div>
     </div>
 

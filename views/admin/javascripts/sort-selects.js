ca848e447069c0491b330a0ea1f00d24e631d73b
Michael Gates <michaelgatesdev@gmail.com>
2020-07-06 02:08:06 -0400

    Fix select sorting duplicate issue

--- views/admin/javascripts/sort-selects.js
+++ views/admin/javascripts/sort-selects.js
@@ -1,22 +1,27 @@
-$(document).ready(() => {
+jQuery(document).ready(() => {
 
     // get currently selected
-    const selectedOption = $('option:selected');
+    const selectedOption = jQuery('option:selected');
     const selectedDropdownIndex = selectedOption[0].index;
     const selectedOptionText = selectedOption[0].text;
 
     // sort the dropdown
-    const select = $('select');
-    select.html(select.find('option').sort(function (x, y) {
-        // to change to descending order switch "<" for ">"
-        return $(x).text() > $(y).text() ? 1 : -1;
-    }));
+    const selects = jQuery('select');
+    for (let select of selects) {
+        select = jQuery(select);
+        const options = jQuery.makeArray(select.find('option'));
+        const sorted = options.sort(function (a, b) {
+            return (jQuery(a).text() > jQuery(b).text()) ? 1 : -1;
+        });
+        select.append(jQuery(sorted)).attr('selectedIndex', 0);
 
-    // select the originally selected item
-    select.get(0).selectedIndex = selectedDropdownIndex === 0 ? 0 : getIndexByText(selectedOptionText);
+        // select the originally selected item
+        select.get(0).selectedIndex = selectedDropdownIndex === 0 ? 0 : getIndexByText(selectedOptionText);
+    }
+
 
     function getIndexByText(text) {
-        const options = $('option');
+        const options = jQuery('option');
         for (const option of options) {
             if (option.text === text) return option.index;
         }

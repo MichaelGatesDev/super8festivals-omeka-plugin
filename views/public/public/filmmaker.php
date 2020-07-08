6eb2b70fd58c09c1b3459059ec861a270e5b4b9e
Michael Gates <michaelgatesdev@gmail.com>
2020-07-08 12:45:04 -0400

    Update filmmaker page to grid layout

--- views/public/public/filmmaker.php
+++ views/public/public/filmmaker.php
@@ -4,7 +4,7 @@ $head = array(
 );
 echo head($head);
 
-
+$photos = get_all_photos_for_filmmaker($filmmaker->id);
 ?>
 
 <section class="container pb-4" id="filmmaker">
@@ -15,35 +15,19 @@ echo head($head);
         </div>
     </div>
 
-    <div class="row mb-5">
+    <div class="row my-5" id="film-catalogs">
         <div class="col">
-            <h3 class="mb-4">Photos</h3>
-            <?php if (count($records = get_all_photos_for_filmmaker($filmmaker->id)) > 0): ?>
-                <?php foreach ($records as $photo): ?>
-                    <div class="card m-4 shadow-sm display-inline-block w-25">
-                        <div class="card-body">
-                            <div class="card-image mb-2" style="background-color: lightgray; height: 250px;">
-                                <img class="card-img-top img-fluid w-100" src="<?= get_relative_path($photo->get_thumbnail_path()); ?>" alt="No preview available"
-                                     style="object-fit: cover; width: 100%; height: 100%;  display: flex; justify-content: center; align-items: center; text-align: center; "/>
-                            </div>
-                            <p class="card-title mb-2">
-                                <span style="font-weight: bold">Title:</span>
-                                <span><?= $photo->title; ?></span>
-                            </p>
-                            <p class="card-title mb-2">
-                                <span style="font-weight: bold">Description:</span>
-                                <span><?= $photo->description; ?></span>
-                            </p>
-                            <p class="card-title mb-2">
-                                <span style="font-weight: bold">Contributor:</span>
-                                <span><?= $photo->contributor; ?></span>
-                            </p>
-                            <a href="<?= get_relative_path($photo->get_path()); ?>" class="stretched-link" data-fancybox="fb-photos" data-title="Editing Super 8 double system"></a>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
+            <h3>
+                Photos (<?= count($photos); ?>)
+            </h3>
+            <?php if (count($photos) == 0): ?>
+                <p>There is no print media available for this festival.</p>
             <?php else: ?>
-                <p>There are no filmmakers here yet.</p>
+                <div class="row row-cols">
+                    <?php foreach ($photos as $film_catalog): ?>
+                        <?= $this->partial("__components/festival-record-card-previewable.php", array('record' => $film_catalog)); ?>
+                    <?php endforeach; ?>
+                </div>
             <?php endif; ?>
         </div>
     </div>

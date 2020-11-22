aa3d1bb47ec35d29607463d00029203169af4542
Michael Gates <michaelgatesdev@gmail.com>
2020-11-22 15:59:46 -0500

    We don't need a new log file every hour, and we also can't fetch contents of a non-existant file

--- helpers/S8FLogger.php
+++ helpers/S8FLogger.php
@@ -30,7 +30,7 @@ function logger_log($level, $msg)
 
     $log_path = get_logs_dir() . "/" . date("Ymd") . ".txt";
 
-    $current = file_get_contents($log_path);
+    $current = file_exists($log_path) ? file_get_contents($log_path) : "";
     $current .= "$timestamp$levelStr: $msg\n";
     file_put_contents($log_path, $current);
 }

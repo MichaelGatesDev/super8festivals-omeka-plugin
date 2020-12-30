65b7402ee919dcc9597a0bf60ece9d8fd806d1e2
Michael Gates <michaelgatesdev@gmail.com>
2020-12-30 16:27:23 -0500

    Only show title upon hovering for image cards

--- views/public/javascripts/components/s8f-card.js
+++ views/public/javascripts/components/s8f-card.js
@@ -3,7 +3,6 @@ import { component } from "../../../shared/javascripts/vendor/haunted.js";
 
 
 import { getAttributeFromElementStr, isEmptyString } from "../../../shared/javascripts/misc.js";
-import { Person } from "../../../admin/javascripts/utils/s8f-records.js";
 
 function S8FCard(element) {
 
@@ -11,17 +10,33 @@ function S8FCard(element) {
 
     let title = "";
     let description = "";
-    let contributor = undefined;
+    let contributor = "";
 
     if (record.file) {
         title = record.file.title;
         description = record.file.description;
-        contributor = record.file.contributor;
+        contributor = record.file.contributor ? Person.getDisplayName(record.file.contributor) : "";
     } else if (record.embed) {
         title = record.embed.title;
         description = record.embed.description;
-        contributor = record.embed.contributor;
+        contributor = record.embed.contributor ? Person.getDisplayName(record.embed.contributor) : "";
     }
+    if (isEmptyString(title)) {
+        title = "Untitled";
+    }
+    if (isEmptyString(description)) {
+        description = "No description available.";
+    }
+    if (isEmptyString(contributor)) {
+        contributor = "N/A";
+    }
+
+    const toggleDetails = (state) => {
+        const elem = element.querySelector(".card-details");
+        if (!elem) return;
+        elem.classList.remove(state ? "hidden" : "visible");
+        elem.classList.add(state ? "visible" : "hidden");
+    };
 
     return html`
         <style>
@@ -40,9 +55,19 @@ function S8FCard(element) {
                 -webkit-box-orient: vertical;
             }
         </style>
-        <div class="card d-inline-block mb-1" style="width: 250px;" id=${element.id}>
+        <div
+            id="${element.id}"
+            class="card d-inline-block mb-1"
+            style="width: 250px;"
+            @mouseenter=${() => { toggleDetails(true); }}
+            @mouseleave=${() => { toggleDetails(false); }}
+        >
             ${record.file ? (html`
-                <a href=${record.file.file_path} data-fancybox=${element.fancyboxId ? `fb-${element.fancyboxId}` : "gallery"} data-caption="${isEmptyString(record.file.description) ? "No description available." : record.file.description}">
+                <a
+                    href=${record.file.file_path}
+                    data-fancybox=${element.fancyboxId ? `fb-${element.fancyboxId}` : "gallery"}
+                    data-caption="${description}"
+                >
                     <img src=${record.file.thumbnail_file_path} class="card-img-top" loading="lazy" alt="" style="height: 200px;">
                 </a>
             `) : record.embed ? html`
@@ -50,25 +75,15 @@ function S8FCard(element) {
                     <iframe class="ratio-item" src="${getAttributeFromElementStr(record.embed.embed, "src")}" allowfullscreen></iframe>
                 </div>
             ` : nothing}
-            <div class="card-body">
-                ${record.person ? html`
-                    <h5 class="card-title text-center" title=${title}>
-                        ${Person.getFullName(record.person)}
-                    </h5>
-                ` : html`
-                    <h5 class="card-title chomp-single" title=${title}>
-                        ${isEmptyString(title) ? "Untitled" : title}
-                    </h5>
-                    <p class="card-text chomp-multi-3" title=${description} style="max-height: 75px;">
-                        ${isEmptyString(description) ? "No description available." : description}
-                    </p>
-                `}
-            </div>
-            ${!record.person ? html`
-                <div class="card-footer">
-                    <p class="chomp-single" title=${contributor ? Person.getDisplayName(contributor.person) : "N/A"}>
-                        Contributor: ${contributor ? Person.getDisplayName(contributor.person) : "N/A"}
-                    </p>
+            ${record.file ? html`
+                <div class="card-details text-center hidden">
+                    <p class="m-0 p-2">${title}</p>
+                </div>
+            ` : record.embed ? html`
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <p class="card-text"><small class="text-muted">Contributor: ${contributor}</small></p>
                 </div>
             ` : nothing}
         </div>

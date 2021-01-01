4d97ec8e1a4fa8ecaa5342775ef6ff9416687fcf
Michael Gates <michaelgatesdev@gmail.com>
2021-01-01 16:34:06 -0500

    Update to use rowcols instead of card-deck as it was removed in newer BS version

--- views/public/public/cities.php
+++ views/public/public/cities.php
@@ -48,36 +48,34 @@ if ($sort_mode == "country") {
         </div>
     </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="card-deck">
-                <?php foreach ($cities as $city): ?>
-                    <?php
-                    $city_loc = $city->get_location();
-                    $banner = $city->get_banner();
-                    if ($banner) {
-                        $banner_file = $banner->get_file();
-                    }
-                    $country = $city->get_country();
-                    $country_loc = $country->get_location();
-                    $country_code = get_country_code($country_loc->name);
-                    ?>
-                    <div class="card d-inline-block m-1" style="width: 15rem;">
-                        <img src="<?= $banner_file ? get_relative_path($banner_file->get_thumbnail_path()) : img("placeholder-200x200.svg") ?>" class="card-img-top" alt="" loading="lazy">
-                        <div class="card-body">
-                            <h5 class="card-title text-capitalize"><?= $city_loc->name; ?></h5>
-                        </div>
-                        <div class="card-footer">
-                            <?php if ($country_code): ?>
-                                <span class="flag-icon flag-icon-<?= strtolower($country_code); ?>"></span>
-                            <?php endif; ?>
-                            <span class="small text-muted m-0 p-0 text-capitalize"><?= $country_loc->name; ?></span>
-                        </div>
-                        <a href="/cities/<?= urlencode($city_loc->name); ?>" class="stretched-link"></a>
+    <div class="row row-cols-auto g-2">
+        <?php foreach ($cities as $city): ?>
+            <?php
+            $city_loc = $city->get_location();
+            $banner = $city->get_banner();
+            if ($banner) {
+                $banner_file = $banner->get_file();
+            }
+            $country = $city->get_country();
+            $country_loc = $country->get_location();
+            $country_code = get_country_code($country_loc->name);
+            ?>
+            <div class="col">
+                <div class="card d-inline-block m-1" style="width: 15rem;">
+                    <img src="<?= $banner_file ? get_relative_path($banner_file->get_thumbnail_path()) : img("placeholder-200x200.svg") ?>" class="card-img-top" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5 class="card-title text-capitalize"><?= $city_loc->name; ?></h5>
                     </div>
-                <?php endforeach; ?>
+                    <div class="card-footer">
+                        <?php if ($country_code): ?>
+                            <span class="flag-icon flag-icon-<?= strtolower($country_code); ?>"></span>
+                        <?php endif; ?>
+                        <span class="small text-muted m-0 p-0 text-capitalize"><?= $country_loc->name; ?></span>
+                    </div>
+                    <a href="/cities/<?= urlencode($city_loc->name); ?>" class="stretched-link"></a>
+                </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
 
 </section>

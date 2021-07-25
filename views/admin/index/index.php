491039a5ec936d7a3e3d8c272b46c57fbb254e58
Michael Gates <michaelgatesdev@gmail.com>
2021-07-25 15:15:44 -0400

    Fix broken urls

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -2,11 +2,11 @@
 
     <div class="row">
         <div class="col">
-            <a href="<?= build_admin_url(["countries"]); ?>" class="btn btn-primary">Countries</a>
-            <a href="<?= build_admin_url(["federation"]); ?>" class="btn btn-primary">Federation</a>
-            <a href="<?= build_admin_url(["filmmakers"]); ?>" class="btn btn-primary">Filmmakers</a>
-            <a href="<?= build_admin_url(["contributors"]); ?>" class="btn btn-primary">Contributors</a>
-            <a href="<?= build_admin_url(["staff"]); ?>" class="btn btn-primary">Site Staff</a>
+            <a href="<?= build_plugin_url(["countries"]); ?>" class="btn btn-primary">Countries</a>
+            <a href="<?= build_plugin_url(["federation"]); ?>" class="btn btn-primary">Federation</a>
+            <a href="<?= build_plugin_url(["filmmakers"]); ?>" class="btn btn-primary">Filmmakers</a>
+            <a href="<?= build_plugin_url(["contributors"]); ?>" class="btn btn-primary">Contributors</a>
+            <a href="<?= build_plugin_url(["staff"]); ?>" class="btn btn-primary">Site Staff</a>
         </div>
     </div>
 
